8638b3b5f33801dc42f3db95f4234cd42b619fe4
andrey-vasenin1 <andrey-vasenin1@live.ru>
2014-05-24 19:23:57 +0400

    Класс Analyser дописан и работоспособен.
    Для работы с классом следует скачать библиотеку JavaCV (https://github.com/bytedeco/javacv) и подключить к проекту следующие файлы: javacv.jar, javacpp.jar, opencv-*.jar, ffmpeg-*.jar и opencv.jar. Вместо звёздочки выбрать тот вариант, который соответсвует системе компьютера.
    
    Необходимо ещё придумать, как отслеживать изменения картинки на экране и делать скриншот...

--- ConvApp/src/Analyser.java
+++ ConvApp/src/Analyser.java
@@ -1,19 +1,12 @@
-import org.neuroph.core.NeuralNetwork;
-import org.neuroph.core.data.DataSet;
-import org.neuroph.imgrec.ColorMode;
-import org.neuroph.imgrec.FractionRgbData;
-import org.neuroph.imgrec.ImageRecognitionHelper;
-import org.neuroph.imgrec.ImageRecognitionPlugin;
-import org.neuroph.imgrec.image.Dimension;
-import org.neuroph.util.TransferFunctionType;
-
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_highgui.cvLoadImage;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_TM_SQDIFF;
+import static org.bytedeco.javacpp.opencv_imgproc.cvMatchTemplate;
 
 /**
  * Created by Андрей on 06.05.2014.
@@ -22,77 +15,97 @@ import java.util.Map;
 // для анализа экрана на наличие изображений.
 // Использовать методы объекта этого класса, а не интерфейса Rel
 
-/* ПОКА ЕЩЁ НИЧЕГО НЕ РАБОТАЕТ */
-
 public class Analyser implements Rel {
 
-    private final String NNET_LABEL = "MainNeuralNetwork";
-    private NeuralNetwork nnet;
     private Robot screenshotRobot;
-    private ImageRecognitionPlugin recognition;
     private String lastFoundName;
-    private BufferedImage lastScreenshot;
+    private IplImage lastScreenshot;
+    private IplImage[] templates;
+    private String[] labels;
+    private int iw, ih, tw, th;
 
     public Analyser() throws java.awt.AWTException {
-        nnet = null;
         lastFoundName = null;
+        iw = 0;
+        ih = 0;
+        tw = 0;
+        th = 0;
         // Инициализация объекта для взятия скриншотов экрана
         screenshotRobot = new Robot(GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice());
     }
 
     @Override
-    public void init(List<Path> imagesPath) {
+    public void init(List<Path> imagesPath) throws IOException {
+        templates = new IplImage[imagesPath.size()];
+        labels = new String[imagesPath.size()];
         // Получение информации об изображениях
-        Dimension dimension = new Dimension(0, 0);
-        List<String> imageLabels = new ArrayList<String>();
-        List<Integer> layerNeuronsCount = new ArrayList<Integer>();
+        int i = 0;
+        for (Path imgPath : imagesPath) {
+            String filename = imgPath.toString();
+            templates[i] = cvLoadImage(filename);
+            int f = filename.lastIndexOf('/');
+            if (f == -1) {
+                f = filename.lastIndexOf('\\');
+            }
+            f++;
+            int l = filename.lastIndexOf('.');
+            labels[i] = filename.substring(f, l);
+            if (tw == 0) {
+                tw = templates[i].width();
+                th = templates[i].height();
+            }
+            i++;
+        }
 
-        // Преобразование изображений в необходимый формат
-        Map<String, FractionRgbData> rgbDataMap = null;
-
-        // Создание нейронной сети
-        nnet = ImageRecognitionHelper.createNewNeuralNetwork(NNET_LABEL,
-                dimension, ColorMode.FULL_COLOR, imageLabels,
-                layerNeuronsCount, TransferFunctionType.SIGMOID);
-
-        // Создание набора для тренировки сети и обучение сети на этом наборе
-        DataSet trainingSet = ImageRecognitionHelper.createTrainingSet(imageLabels, rgbDataMap);
-        nnet.learn(trainingSet);
-
-        recognition = (ImageRecognitionPlugin) nnet.getPlugin(ImageRecognitionPlugin.class);
-
-        lastScreenshot = null;
     }
 
     @Override
     public void screen(int x, int y, int width, int height) {
         lastScreenshot = null;
-        lastScreenshot = screenshotRobot.createScreenCapture(new Rectangle(x, y, width, height));
+        lastScreenshot = IplImage.createFrom(screenshotRobot.createScreenCapture(new Rectangle(x, y, width, height)));
+        iw = lastScreenshot.width();
+        ih = lastScreenshot.height();
+    }
+
+    public void checkImage(String filename) {
+        lastScreenshot = null;
+        lastScreenshot = cvLoadImage(filename);
+        iw = lastScreenshot.width();
+        ih = lastScreenshot.height();
     }
 
     @Override
     public void make() {
-        if (lastScreenshot == null) {
-            lastFoundName = null;
-            return;
-        }
-        // Распознование изображения
-        HashMap<String, Double> output = recognition.recognizeImage(lastScreenshot);
+        int minIndex = -1;
+        double minValue = 0;
+        for (int i = 0; i < templates.length; i++) {
+            IplImage result = cvCreateImage(cvSize(iw - tw + 1, ih - th + 1), IPL_DEPTH_32F, 1);
+            cvMatchTemplate(lastScreenshot, templates[i], result, CV_TM_SQDIFF);
 
-        // Поиск наилучшего соотвествия
-        double maxVal = 0.0;
-        String maxLabel = null;
-        for (String key : output.keySet()) {
-            if (output.get(key) > maxVal) {
-                maxVal = output.get(key);
-                maxLabel = key;
+            double[] minval = new double[2];
+            double[] maxval = new double[2];
+            int[] minlock = new int[2];
+            int[] maxlock = new int[2];
+            cvMinMaxLoc(result, minval, maxval, minlock, maxlock, null);
+
+            if (i == 0) {
+                minValue = minval[0];
+                minIndex = 0;
+            } else if (minval[0] < minValue) {
+                minValue = minval[0];
+                minIndex = i;
             }
         }
-        lastFoundName = maxLabel;
+        lastFoundName = labels[minIndex];
     }
 
     @Override
     public String getName() {
         return lastFoundName;
+    }
+
+    @Override
+    public Dimension getSize() {
+        return new Dimension(tw, th);
     }
 }
--- ConvApp/src/Rel.java
+++ ConvApp/src/Rel.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -10,7 +11,7 @@ public interface Rel {
     // Массив путей к картинкам(путь к папке не могу брать из-за
     // того, что у OpenCv нет функций для анализа папок, есть
     // только загрузка из файла)
-    public void init(List<Path> imagesPath);
+    public void init(List<Path> imagesPath) throws IOException;
 
     // Указываются координаты левого верхнего угла скриншота,
     // ширина и высота выделения
